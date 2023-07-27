5e3afef12b044430916487a4ef35af3d8356f014
OnlyForward0613 <harryborison613@gmail.com>
2023-07-27 18:24:56 +0900

    add: create main structure

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+// load the environment variables from the .env file
+dotenv.config({
+  path: '.env'
+});
+
+/**
+ * Express server application class.
+ * @description Will later contain the routing system.
+ */
+class Server {
+  public app = express();
+}
+
+// initialize server app
+const server = new Server();
+
+// make server listen on some port
+((port = process.env.APP_PORT || 5000) => {
+  server.app.listen(port, () => console.log(`> Listening on port ${port}`));
+})();

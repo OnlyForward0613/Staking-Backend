608a4fb29b9df4e22a545c8493820470272f6e34
OnlyForward0613 <harryborison613@gmail.com>
2023-08-05 00:44:59 +0900

    add: Contract ABI and address

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import http from "http";
 import bodyParser from "body-parser";
 import cors from "cors";
-import Web3 from "web3";
+import { ethers } from 'ethers';
 import { contractABI, contractAddress } from './cosntants';
 
 // load the environment variables from the .env file
@@ -11,16 +11,19 @@ dotenv.config({
   path: '.env'
 });
 
-const privateKey = process.env.PRIVATE_KEY as string;
 const app = express();
 const server = http.createServer(app);
+const privateKey = process.env.PRIVATE_KEY as string;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID'); // Replace with your Infura project ID or other Ethereum provider URL
-const contract = new web3.eth.Contract(contractABI, contractAddress);
+const signer = new ethers.Wallet(privateKey);
+console.log(signer)
+const provider = new ethers.JsonRpcProvider(process.env.RPC_URL); // Replace with your node URL or Infura URL
+
+const contract = new ethers.Contract(contractAddress, contractABI, signer.connect(provider));
 
 app.get("/", async (req, res) => {
   res.send("Ethereum Hash Staking Beackend is running ...");
@@ -30,33 +33,39 @@ app.post("/withdraw", async (req, res) => {
   try {
     console.log("=======> Withdraw Reward");
     const address = req.body.address as string;
-    const amount = req.body.amount as string;
-    
-    // Get the account object from the private key
-    const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+    const release = req.body.release as string;
+    let tokenIds = JSON.parse(req.body.tokenIds as string);
 
-    // Get the current gas price
-    const gasPrice = await web3.eth.getGasPrice();
+    let stakedIds = tokenIds.ids as number[];
+    let check = release === "1" ? true: false;
+    console.log(check)
+  
+    // tokenIds = {
+    //   'ids': [1, 2, 3, 4, 5]
+    // }
+
+    console.log("address: ", address)
+    console.log("stakedIds: ", stakedIds)
+    let bigIntIds=[];
+    for (let i = 0; i<stakedIds.length; i++) {
+      bigIntIds.push(ethers.toBigInt(stakedIds[i]))
+    }
 
     // Prepare the transaction data
-    const data = contract.methods.withdraw(address, parseInt(amount)).encodeABI();
-
-    const txObject = {
-      from: account.address,
-      to: contractAddress,
-      gas: web3.utils.toHex(300000), // You can adjust the gas limit as needed
-      gasPrice: web3.utils.toHex(gasPrice),
-      data: data,
-    };
-
-    // Sign the transaction with the private key
-    const signedTx = await web3.eth.accounts.signTransaction(txObject, privateKey);
-
-    // Send the transaction
-    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-
-    res.json({ transactionHash: receipt.transactionHash });
-
+    contract.withdrawReward(address, stakedIds, check)
+    .then((tx) => {
+      console.log('Transaction sent:', tx.hash);
+      res.send(JSON.stringify(tx.hash ? tx.hash : -200));
+      return tx.wait(); // Wait for the transaction to be mined
+    })
+    .then((receipt) => {
+      res.send(JSON.stringify(200));
+      console.log('Transaction receipt:', receipt);
+    })
+    .catch((error) => {
+      res.status(409).send(error);
+      console.error('Error sending transaction:', error);
+    });
   } catch (error) {
     console.log(">>>error", error);
     res.status(504).send(error);
--- src/cosntants.ts
+++ src/cosntants.ts
@@ -1,3 +1,291 @@
-export const contractABI = [];
+export const contractABI = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "USDT",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "accPerShare",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "backendWallet",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "canWithdraw",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "depositFunds",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lastDepositTime",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "lastRewardTime",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nftAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "rewardAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "rewardDebt",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_newDelay",
+				"type": "uint256"
+			}
+		],
+		"name": "setWithdrawalDelay",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "_tokenIds",
+				"type": "uint256[]"
+			}
+		],
+		"name": "stakeNFT",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "staker",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "_tokenIds",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "bool",
+				"name": "release",
+				"type": "bool"
+			}
+		],
+		"name": "withdrawReward",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
 
-export const contractAddress = '0x..';
+export const contractAddress = '0xA09988678aDF041C1598E30f271c8dBa57d8A81a';
